101b69677497289b48eecf6f068da3fbddcd25cb
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-05-02 02:02:07 +0600

    baseurl problem fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    baseURL: process.env.BETTER_AUTH_SECRET
+    baseURL: process.env.BETTER_AUTH_URL
 })
 
 
